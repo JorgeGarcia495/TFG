6d9b6749d5b9e8aeade2abb3ad8b1a67497d1160
Jorge <jorgegv95@gmail.com>
2019-05-18 00:44:53 +0200

    Fixed dynamic profiling

--- code/modules/profiling/main.py
+++ code/modules/profiling/main.py
@@ -6,24 +6,45 @@
 
 import os
 import time
+import signal
+import shlex
 import subprocess
 import multiprocessing
 
 def multiprocess_func(path, directory):
-    target = directory+path+"/bin/"+path
-    proc = subprocess.Popen('ocount -e INST_RETIRED -i 1 -f temp_file '+target, shell=True)
+    """ Starts the dynamic profiling for a determined period of time
+    """
+    #Path lo store the profiling data
+    temp_directory = directory+path+"/bin/"
+    #Binary to execute
+    target = temp_directory+path
     print('Profiling', path)
-    time.sleep(10)
+    #Metrics/counters to retrieves
+    args = 'ocount -e INST_RETIRED -i 1 -f '+temp_directory+'temp_file'+target
+    #Start of the profiling
+    proc = subprocess.Popen(shlex.split(args), shell=False)
+    time.sleep(40)
+    #Kill subprocess
     proc.kill()
+    try:
+        #Kill childs of the subprocess(Binary execution)
+        pid = int(subprocess.check_output(['pidof', '-s', path]))
+        os.kill(pid, signal.SIGKILL)
+    except subprocess.CalledProcessError:
+        print('Nothing to do')
 
-#TODO: Add documentation
 def run_binaries():
+    """ Executes the existing binary to carry out a dynamic profiling
+    """
     starttime = time.time()
+    #Number of available processors
     processors = multiprocessing.cpu_count()
     print('Number of processors:', processors)
+    #Location of binaries
     directory = '../../results/cg/source_code_paths/'
     files_number = os.listdir(directory)
     
+    #Start of the tasks with parallelism
     with multiprocessing.Pool(processors) as pool:
         processes = [pool.apply_async(multiprocess_func, (file,directory,)) for file in files_number]
         for proc in processes:
