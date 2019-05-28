535d6418e07a9d4cd3960f16a5caac8f685e4992
Jorge <jorgegv95@gmail.com>
2019-05-28 07:55:09 +0200

    Start of plots

--- code/modules/signals_reconstruction/main.py
+++ code/modules/signals_reconstruction/main.py
@@ -5,7 +5,9 @@
 """
 
 import os
+import csv
 import logging
+import pandas as pd
 from . import path_instructions as path_instr
 
 logger = logging.getLogger(__name__)
@@ -67,6 +69,61 @@ def iterate_counters_metrics(location, event):
     except FileNotFoundError as e:
         logger.error(e)
         raise
+
+def get_counters_metrics_per_iteration():
+    paths_directory = '../../results/cg/source_code_paths/'
+    counters = 'temp_file'
+    bin_folder = '/bin/'
+    counters_metrics = []
+    #Counters associated to the cpu to execute the whole application
+    events = ['BR_INST_RETIRED', 'BR_MISS_PRED_RETIRED', 'CPU_CLK_UNHALTED', 'INST_RETIRED', 'LLC_MISSES:0x41', 	'LLC_REFS:0x4f',
+	'arith:fpu_div_active', 	'l1d:0x1', 'l2_rqsts:0x1', 'l2_rqsts:0x3', 'l2_rqsts:0x8', 'l2_rqsts:0x20',	'mem_trans_retired:0x2',	 
+    'mem_uops_retired:all_stores',	'misalign_mem_ref:0x1', 	'misalign_mem_ref:0x2',	'resource_stalls:any', 'uops_dispatched:core']
+    try:
+        #Get paths
+        total_paths = os.listdir(paths_directory)
+    except FileNotFoundError as e:
+        print('La carpeta que contiene los distintos paths no existe')
+        logger.error(e)
+        raise
+    for path in total_paths:
+            path_metrics = get_metrics_per_iteration(path, paths_directory+path+bin_folder+counters, events)
+            counters_metrics.extend(path_metrics[:])
+    return counters_metrics
+
+    dfhsa = pd.read_csv(name, delimiter=',', names=events)
+    dfhsa.index
+    aaaa = pd.DataFrame(counters_metrics)
+    aa = counters_metrics[2][2:]
+    aa[2:100]
+    counters_metrics[0:5]
+        
+def get_metrics_per_iteration(path, location, events):
+    result = []
+    values = []
+    iteration = 0
+    try:
+        with open(location, encoding='utf-8') as file: #Open file containing the dynamic profiling(Counters metrics)
+            for line in file:
+                blank_line = line.strip()
+                if line.startswith('Current time'):
+                    if iteration is not 0:
+                        values.insert(0, path)
+                        values.insert(1, iteration)
+                        result.extend([values[:]])
+                        values.clear()
+                    iteration = iteration + 100
+                elif len(blank_line) > 0 and blank_line.split()[0] in events:
+                    values.append(int(blank_line.split()[1].replace(',', '').strip()))
+#            if len(values) > 0:
+#                values.insert(0, path)
+#                values.insert(1, iteration)
+#                result.extend([values[:]])
+            file.close() #Close file
+            return result
+    except FileNotFoundError as e:
+        logger.error(e)
+        raise
         
 def calculate_mean_counter_metrics(counters_metrics):
     """ Calculates the mean values of the counter metrics for each path
@@ -93,6 +150,17 @@ def calculate_ipc(counters_metrics):
         clock_cycles = values['CPU_CLK_UNHALTED'][0]
         result[key] = inst_retired / clock_cycles
     return result
+
+name = 'pepe.txt'
+data = counters_metrics
+
+def export_paths_metrics(name, data):
+    if os.path.exists(name):
+        os.remove(name)
+    with open(name, 'w') as file:
+        data_writer = csv.writer(file, delimiter=',', dialect='excel')
+        data_writer.writerows(data)
+    file.close()
         
 if __name__ == '__main__':
     main()
