3c4ffd645d3748d80163033ff18b2c0e68404fc9
Jorge Garcia <jorgeg09@ucm.es>
2019-07-09 16:31:28 +0200

    Modifications for parallel profiling

--- code/modules/profiling/main.py
+++ code/modules/profiling/main.py
@@ -11,13 +11,14 @@ import psutil
 import subprocess
 import multiprocessing
 
-def multiprocess_func(path, directory):
+def multiprocess_func(path, directory, binary_name):
     """ Starts the dynamic profiling for a determined period of time
     """
     #Path lo store the profiling data
     temp_directory = directory+path+"/bin/"
+    binary_name = binary_name+'_'+path
     #Binary to execute
-    target = temp_directory+path
+    target = temp_directory+binary_name
     counters = 'CPU_CLK_UNHALTED,INST_RETIRED,LLC_MISSES:0x41,LLC_REFS:0x4f,BR_INST_RETIRED,BR_MISS_PRED_RETIRED,misalign_mem_ref:0x01,misalign_mem_ref:0x02,arith:fpu_div_active,resource_stalls:any,uops_dispatched:core,mem_trans_retired:0x02,mem_uops_retired:all_stores,l1d:0x01,l2_rqsts:0x01,l2_rqsts:0x03,l2_rqsts:0x08,l2_rqsts:0x20'
     print('Profiling', path)
     #Metrics/counters to retrieves
@@ -26,24 +27,25 @@ def multiprocess_func(path, directory):
     try:
         exec_time = time.time()
         proc = subprocess.Popen(shlex.split(args), shell=False)
-        pid = int(subprocess.check_output(['pidof', '-s', path]))
+        pid = int(subprocess.check_output(['pidof', '-s', binary_name]))
         binary_time = float('{}'.format(time.time() - exec_time))
         
         while pid and binary_time < 11:
             time.sleep(5)
             binary_time = float('{}'.format(time.time() - exec_time))
             print('Time for path %s: %d seconds' % (path, binary_time))
-            pid = int(subprocess.check_output(['pidof', '-s', path]))
+            pid = int(subprocess.check_output(['pidof', '-s', binary_name]))
 
         #Kill subprocess
         proc.kill()
+        print('Time limit reached for path %s' % path)
         #Kill childs of the subprocess(Binary execution)
         os.system('kill -9 '+str(pid))
     except subprocess.CalledProcessError:
         print(path, "was finished before the limit time")
         proc.kill()
         
-def run_binaries():
+def run_binaries(binary_name):
     """ Executes the existing binary to carry out a dynamic profiling
     """
     starttime = time.time()
@@ -56,12 +58,8 @@ def run_binaries():
     
     #Start of the tasks with parallelism
     with multiprocessing.Pool(processors) as pool:
-        processes = [pool.apply_async(multiprocess_func, (name,directory,)) for name in files_number]
+        processes = [pool.apply_async(multiprocess_func, (name,directory, binary_name,)) for name in files_number]
         for proc in processes:
             print(proc.get())
 
         print('All paths has been executed in {} seconds'.format(time.time() - starttime))
-        
-    
-if __name__ == '__main__':
-    run_binaries()
